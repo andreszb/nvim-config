6fe95dbfb93debd9f3eeb92a6eca321fcf6e3881
Andres Zambrano <andreszb@gmail.com>
2025-06-25 14:36:52 -0600

    Transparent theme

--- lua/myLuaConf/plugins/init.lua
+++ lua/myLuaConf/plugins/init.lua
@@ -2,6 +2,29 @@ local colorschemeName = nixCats('colorscheme')
 if not require('nixCatsUtils').isNixCats then
   colorschemeName = 'tokyonight-night'
 end
+
+-- Configure tokyonight for transparency
+if colorschemeName == 'tokyonight' or colorschemeName == 'tokyonight-night' then
+  require('tokyonight').setup({
+    style = 'night',
+    transparent = true,
+    terminal_colors = true,
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      functions = {},
+      variables = {},
+      sidebars = 'transparent',
+      floats = 'transparent',
+    },
+    sidebars = { 'qf', 'help', 'terminal', 'packer' },
+    day_brightness = 0.3,
+    hide_inactive_statusline = false,
+    dim_inactive = false,
+    lualine_bold = false,
+  })
+end
+
 vim.cmd.colorscheme(colorschemeName)
 
 -- Setup oil plugin
@@ -18,13 +41,13 @@ require('lze').load({
   { import = 'myLuaConf.plugins.ui.noice' },
   { import = 'myLuaConf.plugins.ui.indent-blankline' },
   { import = 'myLuaConf.plugins.ui.edgy' },
-  
+
   -- Navigation plugins
   { import = 'myLuaConf.plugins.navigation.telescope' },
   { import = 'myLuaConf.plugins.navigation.neo-tree' },
   { import = 'myLuaConf.plugins.navigation.yazi' },
   { import = 'myLuaConf.plugins.navigation.flash' },
-  
+
   -- Editing plugins
   { import = 'myLuaConf.plugins.editing.completion' },
   { import = 'myLuaConf.plugins.editing.multicursor' },
@@ -32,15 +55,15 @@ require('lze').load({
   { import = 'myLuaConf.plugins.editing.ts-comments' },
   { import = 'myLuaConf.plugins.editing.undotree' },
   { import = 'myLuaConf.plugins.editing.grug-far' },
-  
+
   -- Language plugins
   { import = 'myLuaConf.plugins.language.treesitter' },
   { import = 'myLuaConf.plugins.language.treesitter-context' },
   { import = 'myLuaConf.plugins.language.markdown-preview' },
-  
+
   -- Git plugins
   { import = 'myLuaConf.plugins.git.gitsigns' },
-  
+
   -- Tools plugins
   { import = 'myLuaConf.plugins.tools.toggleterm' },
   { import = 'myLuaConf.plugins.tools.trouble' },
@@ -50,7 +73,7 @@ require('lze').load({
   { import = 'myLuaConf.plugins.tools.vim-startuptime' },
   { import = 'myLuaConf.plugins.tools.hardtime' },
   { import = 'myLuaConf.plugins.tools.claude-code' },
-  
+
   -- Misc plugins
   { import = 'myLuaConf.plugins.misc.mini' },
   { import = 'myLuaConf.plugins.misc.snacks' },
